ca2f44efcc0e794921b95e52809bc4e3e86cf582
Alec Gunny <alec.gunny@gmail.com>
2021-01-06 09:13:45 -0800

    fixing model creation checks

--- exportlib/model_repository.py
+++ exportlib/model_repository.py
@@ -283,9 +283,9 @@ class ModelRepository:
         platform: typing.Optional[str] = None,
         force: bool = False,
     ) -> "Model":
-        if any([model.name == name for model in self.models]) and not force:
+        if name in self.models and not force:
             raise ValueError("Model {} already exists".format(name))
-        elif any([model.name == name for model in self.models]) and force:
+        elif name in self.models:
             # append an index to the name of the model starting at 0
             pattern = re.compile(f"{name}_[0-9]+")
             matches = [
